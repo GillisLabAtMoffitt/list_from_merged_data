e2cef6d3316a1a47804b8d8cafef386feb1eb062
chrisleitzinger <christelleleitzinger@gmail.com>
2020-11-09 15:05:41 -0500

    Update fill and checking list

--- script.R
+++ script.R
@@ -10,23 +10,38 @@ DNA_id <-
 
 RNA_id <- 
   readxl::read_xlsx(paste0(path, "/Avatar_MM_Gillis_Linkage_11062020.xlsx")) %>% 
-  select(c("avatar_id", "DNASequencingLibraryID", "RNASequencingLibraryID", "collectiondt"))
+  select(c("avatar_id", "DNASequencingLibraryID", "RNASequencingLibraryID", "collectiondt")) %>% 
+  mutate(RNASequencingLibraryID = str_replace(RNASequencingLibraryID, "unavailable", NA_character_))
 
 
 
 RNA_cleaned <- RNA_id %>% 
   group_by(avatar_id, collectiondt) %>% 
-  fill(DNASequencingLibraryID, RNASequencingLibraryID) %>% 
-  drop_na(RNASequencingLibraryID)
+  fill(DNASequencingLibraryID, RNASequencingLibraryID, .direction = "downup") %>% 
+  fill(RNASequencingLibraryID, DNASequencingLibraryID, .direction = "downup") # Just to make sure but doesn't do anything
 
 
-list <- right_join(RNA_cleaned, DNA_id, by = "avatar_id")
+list <- right_join(RNA_cleaned, DNA_id, by = c("avatar_id", "DNASequencingLibraryID" = "SLID_tumor")) %>% 
+  distinct()
 
-write_csv(list, paste0(path, "/list RNA SLID corresponding to DNA SLID.csv"))
+# write_csv(list, paste0(path, "/list RNA SLID corresponding to DNA SLID.csv"))
+
+# Making sure that the NA in the list are NA in the RNA df
+ID <- list %>% 
+  ungroup() %>% 
+  filter(is.na(RNASequencingLibraryID)) %>% 
+  select(avatar_id)
+
+ID <- left_join(ID, RNA_id, by = "avatar_id")
 
 
+# Making sure that the NA in the list are unavailable in the RNA df
 
+ID1 <- list %>% 
+  ungroup() %>% 
+  filter(RNASequencingLibraryID == "unavailable") %>% 
+  select(avatar_id)
 
-
+ID1 <- left_join(ID1, RNA_id, by = "avatar_id")
 
 
